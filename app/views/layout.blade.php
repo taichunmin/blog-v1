b8f10f771c2bafd18d0e590bc75cba4debaa316e
Chunmin Tai <taichunmin@gmail.com>
2014-07-07 20:39:12 +0800

    add sample nav bar

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -17,7 +17,8 @@
     <![endif]-->
   </head>
   <body>
-    @yield('content')
+  @include('navbar')
+  @yield('content')
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
