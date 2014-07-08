80844c157e3b23ca9a31caee266526f27c286187
Chunmin Tai <taichunmin@gmail.com>
2014-07-09 00:15:31 +0800

    no message

--- app/views/about-me.blade.php
+++ app/views/about-me.blade.php
@@ -3,12 +3,13 @@
 @section('content')
 	<div class="jumbotron">
 		<div class="row">
-			<div class="col-sm-3 col-md-2 text-center">
+			<div class="col-sm-3 text-center">
 				<img src="images/gavatar_20131117.jpg" alt="和風信使" style="width:150px; height:150px" class="img-circle">
 			</div>
-			<div class="col-sm-9 col-md-10">
+			<div class="col-sm-9">
 				<h1>和風信使</h1>
-				<p>從高中因為社團接觸 ACM，然後大學開始自學 PHP。主要是後端工程師，前端略有涉略，美工超級不擅長，請大家多多指教。</p>
+				<p>從高中因為社團接觸 C++ 的 ACM 演算法世界，然後大學開始自學 PHP 以及網頁相關的技術。主要是後端工程師，前端略有涉略，美工超級不擅長，除了程式之外，還喜歡看小說，偶爾陪朋友玩遊戲，換句話說，死宅男一枚。</p>
+				<p>請大家多多指教。</p>
 			</div>
 		</div>
 	</div>
