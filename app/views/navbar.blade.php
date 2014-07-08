188cb4c4927c5bb609843a4ab04555680c73c016
Chunmin Tai <taichunmin@gmail.com>
2014-07-08 16:24:23 +0800

    update icons on navbar

--- app/views/navbar.blade.php
+++ app/views/navbar.blade.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/"><span class="fa fa-home"></span> 筆記國度</a>
+      <a class="navbar-brand" href="/"><span class="fa fa-home fa-lg"></span> 筆記國度</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -16,7 +16,7 @@
       <ul class="nav navbar-nav">
         <li><a href="/about-me"><span class="fa fa-info-circle"></span> 關於我</a></li>
         <li><a href="/experiences"><span class="fa fa-history"></span> 經歷</a></li>
-        <li><a href="/skills"><span class="fa fa-bar-chart-o"></span> 技能</a></li>
+        <li><a href="/skills"><span class="fa fa-bar-chart-o fa-rotate-90"></span> 技能</a></li>
         <li><a href="/works"><span class="fa fa-file-code-o"></span> 作品集</a></li>
         <li><a href="/contact"><span  class="fa fa-phone-square"></span> 聯絡</a></li>
         <li><a href="/about-site"><span class="fa fa-puzzle-piece"></span> 關於本站</a></li>
