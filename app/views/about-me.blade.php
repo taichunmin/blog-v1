666ee3936958439fa9955d941e6583c1d8079880
Chunmin Tai <taichunmin@gmail.com>
2014-07-09 00:46:27 +0800

    Add #about-me-social of /about-me

--- app/views/about-me.blade.php
+++ app/views/about-me.blade.php
@@ -12,6 +12,14 @@
 				<p>從高中因為社團接觸 C++ 的 ACM 演算法世界，然後大學開始自學 PHP 以及網頁相關的技術。主要是後端工程師，前端略有涉略，美工超級不擅長，除了程式之外，還喜歡看小說，偶爾陪朋友玩遊戲，換句話說，死宅男一枚。</p>
 				<p>請大家多多指教。</p>
 			</div>
+			<div id="about-me-social">
+				<a class="btn btn-default btn-lg" href="http://fb.me/taichunmin" target="_blank"><span class="fa fa-facebook-square fa-2x"></span></a>
+				<a class="btn btn-default btn-lg" href="https://twitter.com/taichunmin" target="_blank"><span class="fa fa-twitter fa-2x"></span></a>
+				<a class="btn btn-default btn-lg" href="https://www.google.com/+%E6%88%B4%E5%9D%87%E6%B0%91" target="_blank"><span class="fa fa-google-plus fa-2x"></span></a>
+				<a class="btn btn-default btn-lg" href="https://github.com/taichunmin" target="_blank"><span class="fa fa-github fa-2x"></span></a>
+				<a class="btn btn-default btn-lg" href="http://bitbucket.taichunmin.idv.tw/" target="_blank"><span class="fa fa-bitbucket fa-2x"></span></a>
+				<a class="btn btn-default btn-lg" href="https://trello.com/taichunmin" target="_blank"><span class="fa fa-trello fa-2x"></span></a>
+			</div>
 		</div>
 	</div>
 @stop
