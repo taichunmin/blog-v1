7b2218fa486feacd99c61dd28084043268c951f3
Chunmin Tai <taichunmin@gmail.com>
2014-07-08 16:31:33 +0800

    add  fa-fw to Fix Icons Width

--- app/views/navbar.blade.php
+++ app/views/navbar.blade.php
@@ -14,12 +14,12 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="/about-me"><span class="fa fa-info-circle"></span> 關於我</a></li>
-        <li><a href="/experiences"><span class="fa fa-history"></span> 經歷</a></li>
-        <li><a href="/skills"><span class="fa fa-bar-chart-o fa-rotate-90"></span> 技能</a></li>
-        <li><a href="/works"><span class="fa fa-file-code-o"></span> 作品集</a></li>
-        <li><a href="/contact"><span  class="fa fa-phone-square"></span> 聯絡</a></li>
-        <li><a href="/about-site"><span class="fa fa-puzzle-piece"></span> 關於本站</a></li>
+        <li><a href="/about-me"><span class="fa fa-info-circle fa-fw"></span> 關於我</a></li>
+        <li><a href="/experiences"><span class="fa fa-history fa-fw"></span> 經歷</a></li>
+        <li><a href="/skills"><span class="fa fa-bar-chart-o fa-rotate-90 fa-fw"></span> 技能</a></li>
+        <li><a href="/works"><span class="fa fa-file-code-o fa-fw"></span> 作品集</a></li>
+        <li><a href="/contact"><span  class="fa fa-phone-square fa-fw"></span> 聯絡</a></li>
+        <li><a href="/about-site"><span class="fa fa-puzzle-piece fa-fw"></span> 關於本站</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container -->
