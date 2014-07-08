82dc2e33848a180a02fb603f2c643710b20edbc3
Chunmin Tai <taichunmin@gmail.com>
2014-07-08 23:45:27 +0800

    Add content of /about-me

--- app/views/about-me.blade.php
+++ app/views/about-me.blade.php
@@ -1,5 +1,15 @@
 @extends('layout')
 
 @section('content')
-	Hello World!
+	<div class="jumbotron">
+		<div class="row">
+			<div class="col-md-4">
+				<img src="images/gavatar_20131117.jpg" alt="和風信使">
+			</div>
+			<div class="col-md-8">
+				<h1>和風信使</h1>
+				<p>目前就讀於中興資工，從高中因為社團接觸 ACM，然後大學開始自學 PHP。主要是後端工程師，前端略有涉略，美工超級不擅長，只是一個小小的網站維護工讀生，請大家多多指教。</p>
+			</div>
+		</div>
+	</div>
 @stop
