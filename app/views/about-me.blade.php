57dfef9713e016e569b8c1a09180cbd4a93499e8
Chunmin Tai <taichunmin@gmail.com>
2014-07-09 00:09:12 +0800

    Update content of /about-me

--- app/views/about-me.blade.php
+++ app/views/about-me.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 	<div class="jumbotron">
 		<div class="row">
-			<div class="col-md-4">
+			<div class="col-sm-3 col-md-2 text-center">
 				<img src="images/gavatar_20131117.jpg" alt="和風信使" style="width:150px; height:150px" class="img-circle">
 			</div>
-			<div class="col-md-8">
+			<div class="col-sm-9 col-md-10">
 				<h1>和風信使</h1>
-				<p>目前就讀於中興資工，從高中因為社團接觸 ACM，然後大學開始自學 PHP。主要是後端工程師，前端略有涉略，美工超級不擅長，只是一個小小的網站維護工讀生，請大家多多指教。</p>
+				<p>從高中因為社團接觸 ACM，然後大學開始自學 PHP。主要是後端工程師，前端略有涉略，美工超級不擅長，請大家多多指教。</p>
 			</div>
 		</div>
 	</div>
--- app/views/site-css.blade.php
+++ app/views/site-css.blade.php
@@ -1,3 +1,3 @@
 <style>
-	body { padding-top: 50px; font-family:\5FAE\8EDF\6B63\9ED1\9AD4,Microsoft JhengHei,MingLiU,sans-serif; }
+	body { padding-top: 60px; font-family:\5FAE\8EDF\6B63\9ED1\9AD4,Microsoft JhengHei,MingLiU,sans-serif; }
 </style>
