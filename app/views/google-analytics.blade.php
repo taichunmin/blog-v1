e528643ab77f7e76a2a30ca1c3f938a846be81f2
Chunmin Tai <taichunmin@gmail.com>
2014-07-10 14:29:54 +0800

    Update google-analytics

--- app/views/google-analytics.blade.php
+++ app/views/google-analytics.blade.php
@@ -6,6 +6,8 @@
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-39556213-2', 'auto');
+  ga('require', 'displayfeatures');
+  ga('require', 'linkid', 'linkid.js');
   ga('send', 'pageview');
 
 </script>
