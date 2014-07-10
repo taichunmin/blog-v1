8809749b809ad912a6ab819d7ec0b6cbfedf3d23
Chunmin Tai <taichunmin@gmail.com>
2014-07-10 09:22:27 +0800

    move about-me from content to content-fluid

--- app/views/about-me.blade.php
+++ app/views/about-me.blade.php
@@ -1,25 +1,29 @@
 @extends('layout')
 
-@section('content')
-	<div class="jumbotron">
-		<div class="row">
-			<div class="col-sm-3 text-center">
-				<div class="hidden-xs" style="height:20px">&nbsp;</div>
-				<img src="images/gavatar_20131117.jpg" alt="和風信使" style="width:150px; height:150px" class="img-circle">
+@section('content-fluid')
+	<div class="row" id="about-me-row">
+		<div class="jumbotron">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-3 text-center">
+						<div class="hidden-xs" style="height:20px">&nbsp;</div>
+						<img src="images/gavatar_20131117.jpg" alt="和風信使" style="width:150px; height:150px" class="img-circle">
+					</div>
+					<div class="col-sm-9">
+						<h1>和風信使</h1>
+						<p>從高中因為社團接觸 C++ 的 ACM 演算法世界，然後大學開始自學 PHP 以及網頁相關的技術。主要是後端工程師，前端略有涉略，美工超級不擅長，除了程式之外，還喜歡看小說，偶爾陪朋友玩遊戲，換句話說，死宅男一枚。</p>
+						<p>請大家多多指教。</p>
+					</div>
+					<div id="about-me-social" class="col-sm-9 col-sm-offset-3">
+						<a class="btn btn-lg" href="http://fb.me/taichunmin" target="_blank"><span class="fa fa-facebook-square fa-2x fa-fw"></span></a>
+						<a class="btn btn-lg" href="https://twitter.com/taichunmin" target="_blank"><span class="fa fa-twitter fa-2x fa-fw"></span></a>
+						<a class="btn btn-lg" href="https://www.google.com/+%E6%88%B4%E5%9D%87%E6%B0%91" target="_blank"><span class="fa fa-google-plus fa-2x fa-fw"></span></a>
+						<a class="btn btn-lg" href="https://github.com/taichunmin" target="_blank"><span class="fa fa-github fa-2x fa-fw"></span></a>
+						<a class="btn btn-lg" href="http://bitbucket.taichunmin.idv.tw/" target="_blank"><span class="fa fa-bitbucket fa-2x fa-fw"></span></a>
+						<a class="btn btn-lg" href="https://trello.com/taichunmin" target="_blank"><span class="fa fa-trello fa-2x fa-fw"></span></a>
+					</div>
+				</div>
 			</div>
-			<div class="col-sm-9">
-				<h1>和風信使</h1>
-				<p>從高中因為社團接觸 C++ 的 ACM 演算法世界，然後大學開始自學 PHP 以及網頁相關的技術。主要是後端工程師，前端略有涉略，美工超級不擅長，除了程式之外，還喜歡看小說，偶爾陪朋友玩遊戲，換句話說，死宅男一枚。</p>
-				<p>請大家多多指教。</p>
-			</div>
-		<div id="about-me-social" class="col-sm-9 col-sm-offset-3">
-			<a class="btn btn-lg" href="http://fb.me/taichunmin" target="_blank"><span class="fa fa-facebook-square fa-2x fa-fw"></span></a>
-			<a class="btn btn-lg" href="https://twitter.com/taichunmin" target="_blank"><span class="fa fa-twitter fa-2x fa-fw"></span></a>
-			<a class="btn btn-lg" href="https://www.google.com/+%E6%88%B4%E5%9D%87%E6%B0%91" target="_blank"><span class="fa fa-google-plus fa-2x fa-fw"></span></a>
-			<a class="btn btn-lg" href="https://github.com/taichunmin" target="_blank"><span class="fa fa-github fa-2x fa-fw"></span></a>
-			<a class="btn btn-lg" href="http://bitbucket.taichunmin.idv.tw/" target="_blank"><span class="fa fa-bitbucket fa-2x fa-fw"></span></a>
-			<a class="btn btn-lg" href="https://trello.com/taichunmin" target="_blank"><span class="fa fa-trello fa-2x fa-fw"></span></a>
-		</div>
 		</div>
 	</div>
 @stop
@@ -41,6 +45,9 @@
 		}
 		#about-me-social a.btn:hover{
 			border: 2px solid #000;
+		}
+		#about-me-row{
+			margin-top: -9px;
 		}
 	</style>
 @stop
