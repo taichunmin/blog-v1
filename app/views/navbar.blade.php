f835989e97f07fd5be456aae0100318c632badb9
Chunmin Tai <taichunmin@gmail.com>
2014-07-10 13:10:46 +0800

    navbar 更改站內網址指定方式 改用 url( )

--- app/views/navbar.blade.php
+++ app/views/navbar.blade.php
@@ -8,18 +8,18 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/"><span class="fa fa-home fa-lg"></span> 筆記國度</a>
+      <a class="navbar-brand" href="{{ url('/') }}"><span class="fa fa-home fa-lg"></span> 筆記國度</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="/about-me"><span class="fa fa-info-circle fa-fw"></span> 關於我</a></li>
-        <li><a href="/experiences"><span class="fa fa-history fa-fw"></span> 經歷</a></li>
-        <li><a href="/skills"><span class="fa fa-bar-chart-o fa-rotate-90 fa-fw"></span> 技能</a></li>
-        <li><a href="/works"><span class="fa fa-file-code-o fa-fw"></span> 作品集</a></li>
-        <li><a href="/contact"><span  class="fa fa-phone-square fa-fw"></span> 聯絡</a></li>
-        <li><a href="/about-site"><span class="fa fa-puzzle-piece fa-fw"></span> 關於本站</a></li>
+        <li><a href="{{ url('/about-me') }}"><span class="fa fa-info-circle fa-fw"></span> 關於我</a></li>
+        <li><a href="{{ url('/experiences') }}"><span class="fa fa-history fa-fw"></span> 經歷</a></li>
+        <li><a href="{{ url('/skills') }}"><span class="fa fa-bar-chart-o fa-rotate-90 fa-fw"></span> 技能</a></li>
+        <li><a href="{{ url('/works') }}"><span class="fa fa-file-code-o fa-fw"></span> 作品集</a></li>
+        <li><a href="{{ url('/contact') }}"><span  class="fa fa-phone-square fa-fw"></span> 聯絡</a></li>
+        <li><a href="{{ url('/about-site') }}"><span class="fa fa-puzzle-piece fa-fw"></span> 關於本站</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container -->
